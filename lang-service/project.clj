a5a00845f270a61060b671f9f6cdeb3efe70c148
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2014-04-10 17:50:02 +0000

    command line argument parsing

--- lang-service/project.clj
+++ lang-service/project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.cli "0.3.1"]
+                 [com.cybozu.labs/langdetect "1.1-20120112"]]
   :main ^:skip-aot lang-service.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- lang-service/src/lang_service/core.clj
+++ lang-service/src/lang_service/core.clj
@@ -3,8 +3,22 @@
   (:require [clojure.tools.cli :refer [parse-opts]]
             [lang-service.detect :as detect]))
 
+(def cli-opts
+  [["-H" "--host HOST" "hostname for the message broker"]
+   ["-p" "--port PORT" "port the message borker listens on"
+    :default 5672 :parse-fn #(Integer/parseInt %1) 
+    :validate [#(< 0 %1 < 65536) "a port number must be between 0 and 65536"]]
+   ["-q" "--queue QUEUE" "name of the queue from which the texts are pumped"
+    :default "lang_service_queue"]])
+
 (defn -main
   "sets up the service and accept messages"
   [& args]
-  nil)
-
+  (let [{:keys [options arguments summary errors]} (parse-opts args cli-opts)]
+    (when-not (options :host)
+      (println (str "You must specify the hostname of a message broker. " 
+               "Please see below usage instruction and try again."))
+      (println summary)
+      (System/exit -1))
+    (doseq [[k v] options]
+      (println (str k " " v)))))
