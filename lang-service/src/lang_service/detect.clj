7c0dad4003cddf0fbdc89375fb11a41aa1820a2d
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2014-04-15 13:55:39 +0000

    update

--- lang-service/src/lang_service/detect.clj
+++ lang-service/src/lang_service/detect.clj
@@ -11,17 +11,26 @@
   "return the most probable language of the given text."
   [text]
   (let [detector (DetectorFactory/create)]
-    (.append detector text)
-    (.detect detector)))
+    (try
+      (.append detector text)
+      (.detect detector)
+    (catch com.cybozu.labs.langdetect.LangDetectException e
+      "UNKNOWN"))))
+
 
 (defn detect-probabilities
   "return the list of lang,probabilities pair for the given text"
   [text]
   (let [detector (DetectorFactory/create)
-        probabilities (do
+        probabilities (try
                         (.append detector text)
-                        (.getProbabilities detector))]
-    (map (fn [x] [(.lang x) (.prob x)]) probabilities)))
+                        (.getProbabilities detector)
+                      (catch com.cybozu.labs.langdetect.LangDetectException e
+                        (println (format "problem with %s" text))
+                        nil))]
+    (if (nil? probabilities)
+      [[0 "UNKNOWN"]]
+      (map (fn [x] [(.prob x) (.lang x)]) probabilities))))
 
 (defn detect-error
   "return an error message when the function name passed is invalid"
